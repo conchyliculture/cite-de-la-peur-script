800be705bb876de6774dcb208307d6fe598687ee
Skol Ofensru <6418006+conchyliculture@users.noreply.github.com>
2024-04-04 15:49:38 +0200

    update convert

--- convert.sh
+++ convert.sh
@@ -3,14 +3,17 @@ if [ ! -d venv ]; then
 fi
 source venv/bin/activate
 
-pip install screenplain
-pip install 'screenplain[PDF]'
-
+pip3 show screenplain 1>/dev/null
+if [ $? != 0 ]; then
+  pip install screenplain
+  pip install 'screenplain[PDF]'
+fi
 
 screenplain  --format html script.fountain output/script.html
 screenplain  --format pdf script.fountain output/script.pdf
 
+git commit output/script.html output/script.pdf -m "auto-gen output files"
 git checkout gh-pages
 git merge main
 git checkout main
-git push 
+git push --all origin 
